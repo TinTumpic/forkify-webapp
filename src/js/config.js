e3157896ed8af99af292e6e30867c17553410824
TinTumpic <tumpic.tin@gmail.com>
2025-02-25 15:16:07 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SECONDS = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '92ea5e43-911a-4731-80a2-b1acc2fab86a';
+export const MODAL_CLOSE_SEC = 2.5;
